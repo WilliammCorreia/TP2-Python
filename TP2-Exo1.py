09b95cb683a43c3a31fea36a82fadc3297329648
William <128689649+WilliammCorreia@users.noreply.github.com>
2023-10-07 19:43:02 +0200

    Add files via upload

---
+++ TP2-Exo1.py
@@ -0,0 +1,11 @@
+# CORREIA William Bachelor 2 Groupe 3 - TP2 Exercice 1
+
+valeur = []
+
+valeur.append(int(input("Inserer une valeur :")))
+valeur.append(int(input("Inserer une autre valeur :")))
+
+if (valeur[0] == valeur[1]):
+    print("Elles sont égales")
+else:
+    print("Elles sont différentes")
